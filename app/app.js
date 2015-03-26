a3963df54d6ec011922ddecf92aecb909c952bdb
artsoroka <githubrepository@yandex.ru>
2015-03-26 16:34:38 -0400

    updated app and temaplate file, now using vk api photos search

--- app/app.js
+++ app/app.js
@@ -1,12 +1,43 @@
 var express = require('express'); 
 var app		= express(); 
 var fs 		= require('fs'); 
+var api 	= express.Router(); 
+var request = require('request'); 
 
 app.use(express.static(__dirname + '/public/')); 
 
 var mainPage = fs.readFileSync(__dirname + '/public/index.htm'); 
 
-app.get('/', function(req,res){
+api.get('/photos', function(req,res){
+	request
+	  .get('https://api.vk.com/method/photos.search?v=5.29&count=100&lat=' + req.query.lat + '&long=' + req.query.long)
+	  .on('error', function(err) {
+	    console.log(err)
+	  })
+	  .pipe(res); 
+
+	var data = [
+		{
+			name: 'test', 
+			lat: 59.9225865755825,
+			long: 30.36003440003231
+		},
+		{
+			name: 'test2',
+			lat: 59.9215865755825,
+			long: 30.37003440003231
+		},
+	]
+//	res.end(JSON.stringify(data))
+}); 
+
+api.get('*', function(req,res){
+	res.status(404).json({error: 'no endpoint found'})
+});
+
+app.use('/api', api); 
+
+app.get('*', function(req,res){
 	res.end(mainPage);  
 }); 
 
