80ee3b143cca400f6deea82328de320c347d46dd
artsoroka <githubrepository@yandex.ru>
2015-04-13 13:23:46 -0400

    added trus proxy parameter to get real ip behind the proxy

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-var app = require('./app/app.js'); 
-var port = process.env.GEOSMM_PORT || 8080;  
+var express = require('express'); 
+var server  = express(); 
+var app     = require('./app/app.js'); 
+var port    = process.env.GEOSMM_PORT || 8080;  
 
-app.listen(port);  
+server.enable('trust proxy'); 
+
+server.use(function(req,res,next){
+  console.log('CLIENT IP: ' + req.ip ); 
+  next(); 
+}); 
+
+server.use('/', app); 
+
+server.listen(port);  
 console.log('app is listening on a port: ' + port); 
